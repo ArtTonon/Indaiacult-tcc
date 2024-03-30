bb3dac012de606694536cbab285b2aec57ce3622
ArtTonon <arthurtsilva12@gmail.com>
2024-03-30 12:50:44 -0300

    VITÓRIA

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const LoginButton = () => {
+const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+  window.location.href = '/login';
+};
+
+const LoginButton: React.FC = () => {
   return (
-    <div>
-          <button className="bg-lightblue hover:bg-darkblue text-white font-bold py-2 px-8 rounded-full">
-            Login
-          </button>
-    </div>
+    <button onClick={handleClick} className="bg-lightblue hover:bg-darkblue text-white font-bold py-2 px-8 rounded-full">
+      Login
+    </button>
   );
 };
 
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 
 const SignUp: React.FC = () => {
   const [name, setName] = useState("");
+  const [telephone, setTelephone] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
@@ -13,13 +14,15 @@ const SignUp: React.FC = () => {
 
   const handleSignUp = async () => {
     try {
-      const response = await axios.post("http://localhost:5175/registrar", {
+      const response = await axios.post("http://localhost:5173/registrar", {
         name,
         email,
-        password,
+        telephone,
+        password
       });
       console.log(response.data);
       setName("");
+      setTelephone("");
       setEmail("");
       setPassword("");
       setSuccessMessage("Cadastro realizado com sucesso");
@@ -31,50 +34,107 @@ const SignUp: React.FC = () => {
   };
 
   return (
-    <div>
-      <div className="flex flex-col gap-9 items-center justify-center w-full">
-        <h1 className="text-4xl font-bold">Cadastre-se</h1>
-        <p>
-          Já possui uma conta? Faça{" "}
-          <a href="/login" className="text-lightblue">
-            Login
-          </a>
-        </p>
+    <>
+  <link
+    rel="stylesheet"
+    href="https://unicons.iconscout.com/release/v2.1.9/css/unicons.css"
+  />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+  />
+  <link rel="stylesheet" href="" />
+  <div>
+    <ul className="circles">
+      <li/><li/><li/><li/><li/><li/>
+      <li/><li/><li/><li/><li/><li/>
+      <li/><li/><li/><li/></ul>
+  </div>
+  <div className="section">
+    <div className="container">
+      <div className="row full-height justify-content-center">
+        <div className="col-12 text-center align-self-center py-5">
+          <div className="section pb-5 pt-5 pt-sm-2 text-center">
+            <h6 className="mb-0 pb-3">
+              <span>Logar </span>
+              <span>Inscrever</span>
+            </h6>
+            <input
+              className="checkbox"
+              type="checkbox"
+              id="reg-log"
+              name="reg-log"
+            />
+            <label htmlFor="reg-log" />
+            <div className="card-3d-wrap mx-auto">
+              <div className="card-3d-wrapper">
+                <div className="card-back">
+                  <div className="center-wrap">
+                    <div className="section text-center">
+                    <form className="flex flex-col items-center gap-8 justify-center w-full">
+                      {error && <div className="text-red-500 mb-4">{error}</div>}
+                      {succesMessage && (
+                        <div className="text-geen-500 mb-4">{succesMessage}</div>
+                    )}
+                      <h4 className="mb-3 pb-3">Inscreva-se</h4>
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          className="form-style"
+                          placeholder="Nome Completo"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                        <i className="input-icon uil uil-user" />
+                      </div>
+                      <div className="form-group mt-2">
+                        <input
+                          type="tel"
+                          className="form-style"
+                          placeholder="Telefone"
+                          value={telephone}
+                          onChange={(e) => setTelephone(e.target.value)}
+                        />
+                        <i className="input-icon uil uil-phone" />
+                      </div>
+                      <div className="form-group mt-2">
+                        <input
+                          type="email"
+                          className="form-style"
+                          placeholder="Email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <i className="input-icon uil uil-at" />
+                      </div>
+                      <div className="form-group mt-2">
+                        <input
+                          type="password"
+                          className="form-style"
+                          placeholder="Senha"
+                          value={password}
+                          onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <i className="input-icon uil uil-lock-alt" />
+                      </div>
+                      <button
+                        onClick={handleSignUp}
+                        className="bg-lightblue w-72 py-3 hover:bg-darkblue text-white rounded-md"
+                        >
+                          Criar conta
+                      </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <form className="flex flex-col items-center gap-8 justify-center w-full">
-        {error && <div className="text-red-500 mb-4">{error}</div>}
-        {succesMessage && (
-          <div className="text-geen-500 mb-4">{succesMessage}</div>
-        )}
-        <input
-          type="text"
-          placeholder="Insira seu nome"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          className="border border-white border-b-lightblue mt-16 outline-none w-72"
-        />
-        <input
-          type="email"
-          placeholder="Insira seu Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border border-white border-b-lightblue mt-16 outline-none w-72"
-        />
-        <input
-          type="password"
-          placeholder="Crie uma senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border border-white border-b-lightblue mt-16 outline-none w-72"
-        />
-        <button
-          onClick={handleSignUp}
-          className="bg-lightblue w-72 py-3 hover:bg-darkblue text-white rounded-md"
-        >
-          Criar conta
-        </button>
-      </form>
     </div>
+  </div>
+</>
   );
 };
 
