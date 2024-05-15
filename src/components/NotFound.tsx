39732ad93b5b1c93c42f26e7286479234997fc12
unknown <35417@fiecalunos.lan>
2024-05-15 19:37:50 -0300

    Página de erro ajustada

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -13,12 +13,12 @@ export default function NotFound() {
         src={darkMode 
           ? "../../public/assets/undraw_landing_dark.svg" 
           : "../../public/assets/undraw_landing_light.svg"}
-        className="object-cover w-full lg:w-[512px]"
+        className="object-cover w-full lg:w-[450px]"
       />
-      <h1 className="font-prompt text-2xl dark:text-white">
+      <h1 className="font-prompt text-xl ml-8 dark:text-white">
         A página requisitada não foi encontrada.
       </h1>
-      <button className="py-4 px-8 font-bold rounded-lg bg-lightblue dark:bg-main text-white text-xl hover:bg-darkblue dark:hover:bg-darkblue" >
+      <button className="py-4 px-8 font-bold rounded-lg bg-lightblue dark:bg-main text-white  hover:bg-darkblue dark:hover:bg-darkblue" >
         <a href="/">Voltar ao início</a>
       </button>
     </div>
